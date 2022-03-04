43ec3834a51015493f6c2bf0483a469260ec975c
shamskhodary <shams.khodary@gmail.com>
2022-03-05 01:26:13 +0200

    make changes on js file and readme relates #9

--- README.md
+++ README.md
@@ -13,7 +13,11 @@ Todo list app is an application that allows the user to organize the daily tasks
 The user will be able to add his to-do to the list and save it in local storage, edit his to-dos, and remove any to-do from the list. Most importantly, closing the browser window will not omit what the user planned.
 
 ## How do I get the project on my local machine?
+- Go to the repository and click on the button named 'Code'.
+- Copy the given HTTP link.
+- Open the Git bash in the file you want to place the project.
+- Write the command 'git clone' and then paste the link you copied and Enter. You now have the project on your computer. 
 
 ## Team members:
 
-- Shams Elkhoudary (https://github.com/shamskhodary)
+- [Shams Elkhoudary] (https://github.com/shamskhodary)
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const todoIcon = document.querySelector(".fa-plus");
 const todoList = document.querySelector(".todoList");
 const todoInput = document.querySelector(".todo-input");
+const editBtn = document.querySelector(".new-btn1");
 
 todoIcon.addEventListener("click", clickOnPlus);
-todoList.addEventListener("click", removal);
-
+todoList.addEventListener("click", deleteElement);
+// editBtn.addEventListener("click", editText);
 document.addEventListener("DOMContentLoaded", getTodoListOnLoad);
 
 //* Click on plus button
@@ -20,74 +21,78 @@ function clickOnPlus(e) {
     addList.innerText = todoInput.value;
     todoDiv.appendChild(addList);
 
-
     //* Add edit button
     let addEdit = document.createElement("button");
     addEdit.textContent = "Edit";
     addEdit.classList.add("new-btn1");
     todoDiv.appendChild(addEdit);
 
-  
     //* Add remove button
     let addBtn = document.createElement("button");
     addBtn.textContent = "Remove";
     addBtn.classList.add("new-btn2");
     todoDiv.appendChild(addBtn);
-
     saveToLS(todoInput.value);
     todoInput.value = "";
-
-
-
-    //* Remove an item from the list
-    function removal(e) {
-      if (e.target.classList.contains('new-btn2')){
-        div.remove();
-        deleteFromLocalStorage(tasks);
-            }
-    }
-
-    //* Delete from local storage
-    function deleteFromLocalStorage(tasks){
-      let todos = JSON.parse(localStorage.getItem("todos"));
-      let index = todos.indexOf(tasks);
-      tasks.splice(index, 1)
-      localStorage.setItem('todos', JSON.stringify(tasks))
-    }
-
-
-    //* Edit an item in the list
-    const editBtn = document.querySelector(".new-btn1");
-    editBtn.addEventListener("click", editText)
   }
 }
+
+//* Edit an item in the list
 //* Save to local storage
 function saveToLS(value) {
-  let todoTasks = JSON.parse(localStorage.getItem('todos'));
+  let todoTasks = JSON.parse(localStorage.getItem("todos"));
   if (todoTasks) {
     todoTasks.push(value);
-    localStorage.setItem('todos', JSON.stringify(todoTasks));
+    localStorage.setItem("todos", JSON.stringify(todoTasks));
   } else {
     let todoTasks = [];
     todoTasks.push(value);
-    localStorage.setItem('todos', JSON.stringify(todoTasks));
+    localStorage.setItem("todos", JSON.stringify(todoTasks));
   }
+}
+
+//* Remove an item from the list
+function deleteElement(e) {
+  if (e.target.classList.contains("new-btn2")) {
+    e.target.parentElement.remove();
+    let value = e.target.parentElement.textContent;
+    deleteFromLocalStorage(value);
+  }
+}
+
+//* Delete from local storage
+function deleteFromLocalStorage(value) {
+  let tasksToDo = JSON.parse(localStorage.getItem("todos"));
+  let index = tasksToDo.indexOf(value);
+  tasksToDo.splice(index, 1);
+
+  localStorage.setItem("todos", JSON.stringify(tasksToDo));
 }
 
 //* Reload the page
 function getTodoListOnLoad() {
   if (localStorage.getItem("todos")) {
     let todos = JSON.parse(localStorage.getItem("todos"));
-  
 
-  todos.forEach((todo) => {
-    let todoDiv = document.createElement("div");
-    todoList.appendChild(todoDiv);
+    todos.forEach((todo) => {
+      let todoDiv = document.createElement("div");
+      todoList.appendChild(todoDiv);
 
-    let addList = document.createElement("li")
-    addList.innerText = todo
-    todoDiv.appendChild(addList)
+      let addList = document.createElement("li");
+      addList.innerText = todo;
+      todoDiv.appendChild(addList);
+      //* Add edit button
+      let addEdit = document.createElement("button");
+      addEdit.textContent = "Edit";
+      addEdit.classList.add("new-btn1");
+      todoDiv.appendChild(addEdit);
 
-  });
-}
+      //* Add remove button
+      let addBtn = document.createElement("button");
+      addBtn.textContent = "Remove";
+      addBtn.classList.add("new-btn2");
+      todoDiv.appendChild(addBtn);
+
+    });
+  }
 }
